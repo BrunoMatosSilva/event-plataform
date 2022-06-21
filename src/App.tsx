45aee06981b71f49b944555c574c999c71072639
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-06-21 15:44:41 -0300

    aula 01 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSIONS_QUERY = gql`
+query {
+  lessons{
+    id
+    title
+  }
+}
+`
+
+interface LessonProps {
+  id: string;
+  title: string;
+}
 
 function App() {
+  const { data } = useQuery<{ lessons: LessonProps[] }>(GET_LESSIONS_QUERY)
+
+  console.log(data);
 
   return (
-    <h1>Hello World!</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
