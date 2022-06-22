40e6a4d2a02e0424ca62e405e403ac0e5124b855
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-06-22 18:35:42 -0300

    header & sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSIONS_QUERY = gql`
-query {
-  lessons{
-    id
-    title
-  }
-}
-`
-
-interface LessonProps {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: LessonProps[] }>(GET_LESSIONS_QUERY)
 
-  console.log(data);
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
