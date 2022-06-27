7a106396fb0b68216ad8192ce0b77e3e134d2f09
Bruno Matos Silva <bmsdevfullstack@gmail.com>
2022-06-27 17:46:40 -0300

    Correct Subiscribe text

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -36,7 +36,7 @@ export function Subscribe() {
                     <Logo />
 
                     <h1 className="mt-8 text-[2.5rem] leading-tight">
-                        Construa uma <strong className="text-blue-500">Back-end completo </strong>do zero, com <strong className="text-blue-500"> React e NodeJS</strong>
+                        Construa uma aplicação com <strong className="text-blue-500">Back-end completo </strong>do zero, com <strong className="text-blue-500"> React e NodeJS</strong>
                     </h1>
                     <p className="mt-4 text-gray-200 leading-relaxed">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
