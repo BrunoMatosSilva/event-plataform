871a980f2d188f8a67da1c1b995eb3a41a73b631
Bruno Matos Silva <bmsdevfullstack@gmail.com>
2022-06-27 10:46:43 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export function Header() {
                 <header className="sm:w-full py-5 flex w-full justify-between px-4 items-center sm:justify-center bg-gray-700">
                     <Logo />
 
-                    <Popover.Button className="flex items-center ml-2" >
+                    <Popover.Button className="flex items-center ml-2 sm:hidden" >
                         Aulas {!modalOpen ? <List size={32} className="sm:hidden cursor-pointer text-blue-500" aria-hidden="true" onClick={() => setModalOpen(true)} />
                             : <X size={32} className="sm:hidden cursor-pointer text-blue-500" onClick={() => setModalOpen(false)} />}
                     </Popover.Button>
